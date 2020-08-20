b6e1f9b0871ef41c14280984033696fa696a379e
Igor <iem@cin.ufpe.br>
2020-08-20 19:20:05 -0300

    criado tabela de eventos para trabalhar com a agenda

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from core.models import Evento #importou de core.models (o caminho) a class Evento
 
 # Register your models here.
+
+class EventoAdmin(admin.ModelAdmin):
+    list_display = ('titulo', 'data_evento', 'data_criacao') #para aparecer essas opçoes na listagem
+    list_filter = ('titulo','usuario','data_evento') #cria filtros do lado direito
+admin.site.register(Evento, EventoAdmin)
--- core/models.py
+++ core/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+#criar a tabela Eventos com os fields titulo, data do evento, etc
+class Evento(models.Model):
+    titulo = models.CharField(max_length=100)
+    descricao = models.TextField(blank=True, null=True) #podeser nulo
+    data_evento = models.DateTimeField(verbose_name='Data do Evento') #nao pode ser nulo
+    data_criacao = models.DateTimeField(auto_now=True, verbose_name='Data da Criação') #automaticamente insere a hora 
+    usuario = models.ForeignKey(User, on_delete = models.CASCADE) #vamos usar os usuarios do django admin que criamos
+    #on_delete = models.CASCADE se o usuario for excluido exclui tb todos os eventos dele
+    #python3 manage.py sqlmigrate core 0002
+    #python3 manage.py migrate core 0002
+#para fazer com que a classe vire uma tabela no banco de dados
+#python3 manage.py makemigrations
+#python3 manage.py makemigrations core -> olha so pro app core
+#python3 manage.py sqlmigrate core 0001
+
+    class Meta:
+        db_table = 'evento' #exigindo que a tabela se chame evento
+    
+#python3 manage.py migrate core 0001-> vai fazer as creates tables
+
+#agora vamo registrar a tabela no admin.py
+
+    def __str__(self):
+        return self.titulo #agora vai aparecer o titulo do evento correto
